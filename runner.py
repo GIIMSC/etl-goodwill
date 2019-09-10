3c888b4e8ee010396dc468ca755f5c8059b37c3e
Regina Compton <reginafcompton@gmail.com>
2019-09-10 16:39:44 -0400

    chore: Sort imports

--- runner.py
+++ runner.py
@@ -1,8 +1,8 @@
+from config.config import (GOOGLE_DRIVE_CREDENTIALS, SPREADSHEET_ID,
+                           SQLALCHEMY_DATABASE_URI)
 from etl.extractor import Extractor
-from etl.transformer import Transformer
 from etl.loader import Loader
-
-from config.config import GOOGLE_DRIVE_CREDENTIALS, SPREADSHEET_ID, SQLALCHEMY_DATABASE_URI
+from etl.transformer import Transformer
 
 sheet_as_list = Extractor(GOOGLE_DRIVE_CREDENTIALS, SPREADSHEET_ID).get_sheet_as_list()
 dataframe = Transformer(sheet_as_list).clean_dataframe()
