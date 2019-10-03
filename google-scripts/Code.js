877040336ebb9f92539c2f50da4902e8c90b3706
Regina Compton <reginafcompton@gmail.com>
2019-10-03 09:44:09 -0400

    task: Move files from Goodwilldatainitiative

---
+++ google-scripts/Code.js
@@ -0,0 +1,110 @@
+function onSubmit(e) {
+  console.info(e.namedValues);
+  Logger.log(e.namedValues);
+  var memberId = String(e.namedValues['Goodwill Member ID']).trim();
+  
+  if (!memberId) {
+    return;
+  }
+  
+  // `getActiveSheet` returns a Sheet object (in this case, the Master sheet)
+  // https://developers.google.com/apps-script/reference/spreadsheet/spreadsheet-app#getactivesheet
+  // https://developers.google.com/apps-script/reference/spreadsheet/sheet.html
+  var masterResponsesSheet = SpreadsheetApp.openById(MASTER_RESPONSES_SHEET).getActiveSheet();
+  var masterHeaders = masterResponsesSheet.getRange(1, 1, 1, masterResponsesSheet.getMaxColumns());  
+  var members = sheetToObjs(getMemberMappingsSheet());
+  var memberSheetId = getMemberSheet(members, memberId);
+
+  var email;
+  var subject;
+  if (memberSheetId) {
+    var uniqueRowIdentifier = create_UUID()
+    var newRowValues = e.values
+    newRowValues.push(uniqueRowIdentifier);
+    var memberSheet = copyToMemberSheet(memberSheetId, masterHeaders, newRowValues);
+
+    subject = "Thank you for submitting the Goodwill Programs Data form";
+    email = "Your responses are shown below. To edit them, go to " + memberSheet.getUrl() + ".";
+  } else {
+    subject = "There has been a problem with your programs form submission";
+    email = "Member id " + memberId + " has not been set up to enable programs data submission. " +
+      "Please check the member ID again. Your responses are shown below.";
+  }
+  email += "<br><br>" + constructEmailBody(FormApp.openByUrl(masterResponsesSheet.getFormUrl()), e.namedValues);
+  
+  MailApp.sendEmail({
+    to: String(e.namedValues['Your email address']).trim(),
+    subject: subject,
+    htmlBody: email,
+    noReply: true
+  });
+}
+
+function getMemberMappingsSheet() {
+  return SpreadsheetApp.openById(MEMBER_MAPPINGS_SHEET);
+}
+
+function sheetToObjs(sheet) {
+  var values = sheet.getDataRange().getValues();
+  
+  var data = [];
+  
+  // Start at row 1 to avoid headers
+  for (var i = 1; i < values.length; i++) {
+    var rowData = {};
+    for (var j = 0; j < values[i].length; j++) {
+      if (values[i][j]) {
+        rowData[values[0][j]] = values[i][j];
+      }
+    }
+    if (Object.keys(rowData).length > 0) {
+      data.push(rowData);
+    }
+  }
+  
+  return data;
+}
+
+function getMemberSheet(members, memberId) {
+  for (var i = 0; i < members.length; i++) {  
+    if (members[i]["Location"] == memberId) {
+      return members[i]["Spreadsheet ID"];
+    }
+  }
+  return null;
+}
+
+function copyToMemberSheet(memberSheetId, masterHeaders, newRow) {
+  var memberSheet = SpreadsheetApp.openById(memberSheetId);
+  var copyHeaders = memberSheet.getActiveSheet().getRange(1, 1, 1, masterHeaders.getNumColumns());
+  copyHeaders.setRichTextValues(masterHeaders.getRichTextValues());
+  
+  memberSheet.appendRow(newRow);
+  return memberSheet;
+}
+
+function constructEmailBody(form, responses) {
+  var items = form.getItems();
+  var email = "";
+  for (var i = 0; i < items.length; i++) {
+    var question = items[i].getTitle();
+    if (question in responses) {
+      email += question + ": " + responses[question] + "<br>";
+    } else {
+      // If it's not in the form response, then it was a section header
+      email += "<b>" + question + "</b><br>";
+    }
+  }
+  return email;
+}
+
+function create_UUID(){
+  // https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
+  var dt = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (dt + Math.random()*16)%16 | 0;
+    dt = Math.floor(dt/16);
+    return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+  });
+  return uuid;
+}
---
+++ google-scripts/test.js
@@ -0,0 +1,52 @@
+function gastTestRunner() {
+  if ((typeof GasTap)==='undefined') { // GasT Initialization. (only if not initialized yet.)
+    eval(UrlFetchApp.fetch('https://raw.githubusercontent.com/zixia/gast/master/src/gas-tap-lib.js').getContentText())
+  } // Class GasTap is ready for use now!
+  
+  var log = "";
+  var loggerFunc = function (msg) { log += msg + "\n" }
+  
+  var test = new GasTap({
+    logger: loggerFunc
+  })
+  
+  testMemberMappingsSheetExists(test);
+  testSheetToObjs(test);
+  testConstructEmailBody(test);
+  
+  test.finish();
+  Logger.log(log);
+  return { failures: test.totalFailed(), log: log }
+}
+
+function testMemberMappingsSheetExists(test) {
+  test('testMemberMappingsSheetExists', function(t) {
+    var sheet = getMemberMappingsSheet();
+    t.ok(sheet, "mappings sheet exists");
+  });
+}
+
+function testSheetToObjs(test) {
+  test('testSheetToObjs', function(t) {
+    var sheet = SpreadsheetApp.openById("19J5yaeojSqrcVcs0yUbj7qZjBsDWcVuX03d8om_yp9I");
+    var objs = sheetToObjs(sheet);
+    
+    t.equal(objs.length, 2, "2 objs created from sheet");
+    t.equal(Object.keys(objs[0]).length, 3, "3 items in row 1")
+    t.equal(objs[0]["Member ID"], "one", "member one");
+    t.equal(objs[0]["Spreadsheet ID"], "139uaTMu3TlfqZLzQB8fI7_5jRkHrEjqLDJ5QAgGp330", "member one ss");
+    t.equal(objs[0]["Location"], "Somewhere, CA", "member one loc");
+    t.notOk(objs[1]["Member ID"], "row 1 missing member id");
+  });
+}
+
+function testConstructEmailBody(test) {
+  test('testConstructEmailBody', function(t) {
+    var testForm = FormApp.openById("1rhs-y4VsRuL9q74-ErLlPOxu_msgVqp6-rv97bcvWSA");
+    var testResponses = {"Goodwill Member ID": "Somewhere, CA", "Format": "In person, Online", "Should this program be available in Google Pathways?": "Yes"};
+    
+    var expectedEmailBody = "<b>Goodwill Info</b><br>Goodwill Member ID: Somewhere, CA<br>Should this program be available in Google Pathways?: Yes<br>Format: In person, Online<br>";
+    
+    t.equal(constructEmailBody(testForm, testResponses), expectedEmailBody, "email matches expected");
+  });
+}
