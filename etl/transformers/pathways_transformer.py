2c9bccee54bae63f9f7386b04e93ccd4b6a863f6
Regina Compton <reginafcompton@gmail.com>
2020-03-24 09:38:42 -0400

    test: Write tests for PathwaysTransformer

--- etl/transformers/pathways_transformer.py
+++ etl/transformers/pathways_transformer.py
@@ -1,3 +1,4 @@
+import re
 import usaddress
 
 from converter import (educational_occupational_programs_converter,
@@ -13,24 +14,25 @@ class PathwaysTransformer(Transformer):
     def _make_prereq_blob(self, row):
         prereq_blob = {}
 
-        if getattr(row, "IsDiplomaRequired"):
-            prereq_blob['credential_category'] = "HighSchool"
+        if getattr(row, 'IsDiplomaRequired'):
+            prereq_blob['credential_category'] = 'HighSchool'
         
-        if getattr(row, "EligibleGroups"):
-            prereq_blob["eligible_groups"] = getattr(row, "EligibleGroups")
+        if getattr(row, 'EligibleGroups'):
+            prereq_blob['eligible_groups'] = getattr(row, 'EligibleGroups')
         
-        if getattr(row, "MaxIncomeEligibility"):
-            prereq_blob["max_income_eligibility"] = getattr(row, "MaxIncomeEligibility")
+        if getattr(row, 'MaxIncomeEligibility'):
+            prereq_blob['max_income_eligibility'] = getattr(row, 'MaxIncomeEligibility')
 
-        if getattr(row, "Prerequisites"):
-            prereq_blob["other_program_prerequisites"] = getattr(row, "Prerequisites")
+        if getattr(row, 'Prerequisites'):
+            prereq_blob['other_program_prerequisites'] = getattr(row, 'Prerequisites')
         
         return prereq_blob
 
     def _make_address_blob(self, row):
         def parse_address(address):
             parsed_address = usaddress.tag(address)[0]
-            street_address = f"{parsed_address.get('AddressNumber')} {parsed_address.get('StreetNamePreDirectional')} {parsed_address.get('StreetName')} {parsed_address.get('StreetNamePostType')}"
+            street_address = f"{parsed_address.get('AddressNumber', '')} {parsed_address.get('StreetNamePreDirectional', '')} {parsed_address.get('StreetName', '')} {parsed_address.get('StreetNamePostType', '')}"
+            street_address = re.sub(' +', ' ', street_address) # Replace double spaces with single space
 
             return {
                 'street_address': street_address, 
@@ -40,8 +42,8 @@ class PathwaysTransformer(Transformer):
             }
 
         provider_address_list = []
-        provider_address = getattr(row, "ProviderAddress")
-        program_address = getattr(row, "ProgramAddress")
+        provider_address = getattr(row, 'ProviderAddress')
+        program_address = getattr(row, 'ProgramAddress')
         
         try:
             parsed_provider_address = parse_address(provider_address)
@@ -62,9 +64,9 @@ class PathwaysTransformer(Transformer):
         for row in self.dataframe.itertuples(index=True, name='Pandas'):
             provider_address = self._make_address_blob(row)
 
-            if getattr(row, "IsPaid") == "Yes":
+            if getattr(row, 'IsPaid') == 'Yes':
                 input_kwargs = {
-                    "program_description": getattr(row, 'ProgramDescription'),
+                    'program_description': getattr(row, 'ProgramDescription'),
                     'program_name': getattr(row, 'ProgramName'),
                     'program_url': getattr(row, 'ProgramUrl'), 
                     'provider_name': getattr(row, 'ProgramProvider'), 
@@ -87,27 +89,24 @@ class PathwaysTransformer(Transformer):
                     input_kwargs['program_prerequisites'] = prereq_blob
 
                 pathways_json_ld = work_based_programs_converter(**input_kwargs)
-
-                print(pathways_json_ld)
-
             else:
                 input_kwargs = {
-                    'application_deadline': getattr(row, "ApplicationDeadline"), 
-                    'program_name': getattr(row, "ProgramName"), 
-                    'offers_price': getattr(row, "TotalCostOfProgram"), 
-                    'program_url': getattr(row, "ProgramUrl"), 
-                    'provider_name': getattr(row, "ProgramProvider"), 
-                    'provider_url': getattr(row, "ProviderUrl"), 
-                    'provider_telephone': getattr(row, "ContactPhone"), 
+                    'application_deadline': getattr(row, 'ApplicationDeadline'), 
+                    'program_name': getattr(row, 'ProgramName'), 
+                    'offers_price': getattr(row, 'TotalCostOfProgram'), 
+                    'program_url': getattr(row, 'ProgramUrl'), 
+                    'provider_name': getattr(row, 'ProgramProvider'), 
+                    'provider_url': getattr(row, 'ProviderUrl'), 
+                    'provider_telephone': getattr(row, 'ContactPhone'), 
                     'provider_address': provider_address, 
                     'time_to_complete': '', # TODO: How are we going to handle ProgramLength? 
-                    'identifier_cip': getattr(row, "CIP"), 
-                    'identifier_program_id': getattr(row, "ProgramId"),   
-                    'start_date': getattr(row, "StartDates"), # I think this is a list?
-                    'end_date': getattr(row, "EndDates"), # I think this is a list?
-                    'occupational_credential_awarded': getattr(row, "CredentialEarned"), 
-                    'maximum_enrollment': getattr(row, "MaximumEnrollment"), 
-                    'time_of_day': getattr(row, "Timing")
+                    'identifier_cip': getattr(row, 'CIP'), 
+                    'identifier_program_id': getattr(row, 'ProgramId'),   
+                    'start_date': getattr(row, 'StartDates'), # I think this is a list?
+                    'end_date': getattr(row, 'EndDates'), # I think this is a list?
+                    'occupational_credential_awarded': getattr(row, 'CredentialEarned'), 
+                    'maximum_enrollment': getattr(row, 'MaximumEnrollment'), 
+                    'time_of_day': getattr(row, 'Timing')
                 }
 
                 prereq_blob = self._make_prereq_blob(row)
@@ -115,5 +114,5 @@ class PathwaysTransformer(Transformer):
                     input_kwargs['program_prerequisites'] = prereq_blob
 
                 pathways_json_ld = educational_occupational_programs_converter(**input_kwargs)
-
-                print(pathways_json_ld)
+            
+            return pathways_json_ld
--- tests/test_patthways_transformer.py
+++ tests/test_patthways_transformer.py
@@ -3,17 +3,67 @@ import pandas as pd
 from etl.transformers.pathways_transformer import PathwaysTransformer
 
 
-def test_make_prereq_blob(transformer):
+def test_make_prereq_blob_all():
     df = pd.DataFrame({
-        "IsDiplomaRequired": ['Yes'],
-        "EligibleGroups": ["Youth"],
-        'MaxIncomeEligibility': ['20000'],
-        'Prerequisites': 'Other'
+        'IsDiplomaRequired': ['Yes'],
+        'EligibleGroups': ['Veteran'],
+        'MaxIncomeEligibility': ['40000'],
+        'Prerequisites': ['Reliable transportation']
     })
-
     transformer = PathwaysTransformer(dataframe=df)
     row = df.iloc[0]
 
     results = transformer._make_prereq_blob(row)
 
-    pass
+    assert results['credential_category'] == 'HighSchool'
+    assert results['eligible_groups'] == 'Veteran'
+    assert results['max_income_eligibility'] == '40000'
+    assert results['other_program_prerequisites'] == 'Reliable transportation'
+
+
+def test_make_prereq_blob_one():
+    df = pd.DataFrame({
+        'IsDiplomaRequired': ['Yes'],
+        'EligibleGroups': [''],
+        'MaxIncomeEligibility': [''],
+        'Prerequisites': ['']
+    })
+    transformer = PathwaysTransformer(dataframe=df)
+    row = df.iloc[0]
+
+    results = transformer._make_prereq_blob(row)
+
+    assert results['credential_category'] == 'HighSchool'
+    assert 'eligible_groups' not in results
+    assert 'max_income_eligibility' not in results
+    assert 'other_program_prerequisites' not in results
+
+def test_make_address_blob_for_provider_and_program():
+    df = pd.DataFrame({
+        'ProviderAddress': ['111 Grickle Grass Lane, Springfield MA 88884'],
+        'ProgramAddress': ['234 Mulberry Street, Chicago IL 74444']
+    })
+    transformer = PathwaysTransformer(dataframe=df)
+    row = df.iloc[0]
+
+    results = transformer._make_address_blob(row)
+    expected_results = [{'street_address': '111 Grickle Grass Lane', 'address_locality': 'Springfield', 'address_region': 'MA', 'postal_code': '88884'}, {'street_address': '234 Mulberry Street', 'address_locality': 'Chicago', 'address_region': 'IL', 'postal_code': '74444'}]
+    
+    assert len(results) == 2
+    assert expected_results == results
+
+def test_make_address_blob_for_provider():
+    df = pd.DataFrame({
+        'ProviderAddress': ['111 Grickle Grass Lane, Springfield MA 88884'],
+        'ProgramAddress': ['']
+    })
+    transformer = PathwaysTransformer(dataframe=df)
+    row = df.iloc[0]
+
+    results = transformer._make_address_blob(row)
+    expected_results = [{'street_address': '111 Grickle Grass Lane', 'address_locality': 'Springfield', 'address_region': 'MA', 'postal_code': '88884'}]
+    
+    assert len(results) == 1
+    assert expected_results == results
+
+    
