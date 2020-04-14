1a70ebd59e5228e7810295bfacb16d2686b9c2ec
Regina Compton <reginafcompton@gmail.com>
2020-04-13 21:17:31 -0400

    task: Update runer to pull from Master Sheet

--- runner.py
+++ runner.py
@@ -1,6 +1,6 @@
 from sqlalchemy import MetaData, Table, create_engine
 
-from config.config import (GOOGLE_DRIVE_CREDENTIALS, SPREADSHEET_IDS,
+from config.config import (GOOGLE_DRIVE_CREDENTIALS, MASTER_SHEET_ID,
                            SQLALCHEMY_DATABASE_URI)
 from etl.extractor import Extractor
 from etl.loader import Loader
@@ -10,29 +10,29 @@ from etl.utils.logger import logger
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
+logger.info('----Running ETL')
 
-for goodwill, spreadsheet_id in SPREADSHEET_IDS.items():
-    logger.info('----Running ETL for {}'.format(goodwill))
+sheet_as_list = Extractor(
+    google_account_info=GOOGLE_DRIVE_CREDENTIALS,
+    spreadsheet_id=MASTER_SHEET_ID
+).get_sheet_as_list()
 
-    sheet_as_list = Extractor(
-        google_account_info=GOOGLE_DRIVE_CREDENTIALS,
-        spreadsheet_id=spreadsheet_id
-    ).get_sheet_as_list()
+if sheet_as_list:
+    dataframe = DataframeTransformer(
+        sheet=sheet_as_list,
+        engine=engine
+    ).transform()
+    logger.info(f'----Initial transformation complete: {len(dataframe)} records prepped for PathwaysTransformer.')
 
-    if sheet_as_list:
-        logger.info('----Data found')
-        dataframe = DataframeTransformer(
-            sheet=sheet_as_list,
-            engine=engine
-        ).transform()
+    pathways_dataframe = PathwaysTransformer(
+        dataframe=dataframe
+    ).pathways_transform()
+    logger.info(f'----Pathways transformation complete: {len(pathways_dataframe)} records prepped for loading.')
 
-        pathways_dataframe = PathwaysTransformer(
-            dataframe=dataframe
-        ).pathways_transform()
-
-        loader = Loader(engine=engine)
-        loader.load_data(
-            dataframe=pathways_dataframe,
-            table_name='pathways_program',
-            primary_key='id'
-        )
+    loader = Loader(engine=engine)
+    loader.load_data(
+        dataframe=pathways_dataframe,
+        table_name='pathways_program',
+        primary_key='id'
+    )
+    logger.info('----Data loaded into Google Pathways API')
