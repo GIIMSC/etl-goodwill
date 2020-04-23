805bad909472142cf0726c09643ef0789f2f8f7c
Regina Compton <reginafcompton@gmail.com>
2020-04-23 10:36:15 -0400

    task: Add remove_programs_not_marked_for_pathways and organize code with OOP

--- etl/pathways_opt_out.py
+++ etl/pathways_opt_out.py
@@ -4,57 +4,88 @@ from sqlalchemy.sql import select
 from etl.utils.utils import make_dataframe_with_headers
 
 
-def remove_deleted_programs(list_of_lists, programs_table, engine):
-    """Delete programs from the database.
+class OptOut:
+    """This class enables the deletion of programs that should NOT be in the
+    Brighthive Google Pathways API.
 
-    This function deletes programs from the database, if those programs
-    no longer reside in the Master Google Sheet. This may happen when a local Goodwill
-    outright deletes a program from their local sheet.
-
-    Args:
-        list_of_lists: all Google sheet data; return value of `get_sheet_as_list` function in Extractor
+    Attributes:
+        google_sheet_as_list: all Google sheet data; return value of `get_sheet_as_list` function in Extractor
+        programs_table: a SQLAlchemy Table object that reflects the `pathways_program` table in the API database
         engine: a SQLAlchemy engine
     """
-    # 1. Get all IDs in the Google sheet by filtering a Pandas dataframe.
-    dataframe = make_dataframe_with_headers(list_of_lists)
-    all_ids_in_sheet = dataframe["Row Identifier (DO NOT EDIT)"].tolist()
 
-    with engine.connect() as connection:
-        # 2. Get all IDs in the Pathways database
-        select_all = select([programs_table])
-        all_programs_in_database = connection.execute(select_all)
-        all_ids_in_database = [program.id for program in all_programs_in_database]
+    def __init__(self, google_sheet_as_list, programs_table, engine):
+        self.google_sheet_as_list = google_sheet_as_list
+        self.programs_table = programs_table
+        self.engine = engine
+        self.dataframe_of_google_sheet = make_dataframe_with_headers(
+            self.google_sheet_as_list
+        )
 
-        # 3. Determine if the database has IDs that the Google does not have
-        programs_to_delete = [
-            program_id
-            for program_id in all_ids_in_database
-            if program_id not in all_ids_in_sheet
+    def remove_deleted_programs(self):
+        """Delete programs from the database.
+
+        This function deletes programs from the database, if those
+        programs no longer reside in the Master Google Sheet. This may
+        happen when a local Goodwill outright deletes a program from
+        their local sheet.
+        """
+        # 1. Get all IDs in the Google sheet by filtering a Pandas dataframe.
+        all_ids_in_sheet = self.dataframe_of_google_sheet[
+            "Row Identifier (DO NOT EDIT)"
+        ].tolist()
+
+        with self.engine.connect() as connection:
+            # 2. Get all IDs in the Pathways database
+            select_all = select([self.programs_table])
+            all_programs_in_database = connection.execute(select_all)
+            all_ids_in_database = [program.id for program in all_programs_in_database]
+
+            # 3. Determine if the database has IDs that the Google does not have
+            programs_to_delete = [
+                program_id
+                for program_id in all_ids_in_database
+                if program_id not in all_ids_in_sheet
+            ]
+
+            # 4. (OPTIONAL) Delete programs from Pathways database
+            if programs_to_delete:
+                delete_object = self.programs_table.delete().where(
+                    self.programs_table.c.id.in_(
+                        program for program in programs_to_delete
+                    )
+                )
+                connection.execute(delete_object)
+
+        return programs_to_delete
+
+    def remove_programs_not_marked_for_pathways(self):
+        """Delete programs from the database.
+
+        This function deletes programs from the database, if those
+        programs do not have the value 'Yes' in the field 'Should this
+        program be available in Google Pathways?'. This may happen when
+        a local Goodwill decides that a program should no longer appear
+        in pathways, and they adjust this preference in their local
+        sheet.
+        """
+        df = self.dataframe_of_google_sheet
+        filtered_df = df[
+            df["Should this program be available in Google Pathways?"] != "Yes"
         ]
 
-        # 4. (OPTIONAL) Delete programs from Pathways database
-        if programs_to_delete:
-            delete_object = programs_table.delete().where(
-                programs_table.c.id.in_(program for program in programs_to_delete)
+        non_pathways_programs = filtered_df["Row Identifier (DO NOT EDIT)"].tolist()
+        # It is possible that a local Goodwill will manually (and erroneously) create an entry, i.e., the program does programmatically get a Row Identifier.
+        non_pathways_programs = [
+            program for program in non_pathways_programs if program is not None
+        ]
+
+        with self.engine.connect() as connection:
+            delete_object = self.programs_table.delete().where(
+                self.programs_table.c.id.in_(
+                    program for program in non_pathways_programs
+                )
             )
             connection.execute(delete_object)
 
-    return programs_to_delete
-
-
-def remove_programs_not_marked_for_pathways(dataframe):
-
-    """
-    1. filter dataframe for "no"
-
-    self.dataframe[
-        self.dataframe["Should this program be available in Google Pathways?"]
-        == "Yes"
-    ]
-
-    2. get the IDs
-
-    3. delete those IDs, if they exist, from the pathways database.
-    """
-
-    return
+        return non_pathways_programs
--- runner.py
+++ runner.py
@@ -10,10 +10,7 @@ from etl.loader import Loader
 from etl.transformers.dataframe_transformer import DataframeTransformer
 from etl.transformers.pathways_transformer import PathwaysTransformer
 from etl.utils.logger import logger
-from etl.pathways_opt_out import (
-    remove_deleted_programs,
-    remove_programs_not_marked_for_pathways,
-)
+from etl.pathways_opt_out import OptOut
 
 
 """This ETL process accesses the Pathways database via a SQLAlchemy MetaData object, which describes the
@@ -32,12 +29,17 @@ sheet_as_list = Extractor(
 ).get_sheet_as_list()
 
 if sheet_as_list:
-
-    deleted_records = remove_deleted_programs(
-        list_of_lists=sheet_as_list, programs_table=programs_table, engine=engine
+    opt_out = OptOut(
+        google_sheet_as_list=sheet_as_list, programs_table=programs_table, engine=engine
     )
+    deleted_records = opt_out.remove_deleted_programs()
     logger.info(
         f"----Deleted {len(deleted_records)} records from the Pathways database. {' '.join(deleted_records)}"
+    )
+    opt_out_records = opt_out.remove_programs_not_marked_for_pathways()
+
+    logger.info(
+        f"----Found {len(opt_out_records)} records that 'opt-out' of Pathways. {' '.join(opt_out_records)}"
     )
 
     dataframe = DataframeTransformer(sheet=sheet_as_list, engine=engine).transform()
@@ -52,6 +54,6 @@ if sheet_as_list:
 
     loader = Loader(engine=engine)
     loader.load_data(
-        dataframe=pathways_dataframe, table_name="pathways_program", primary_key="id"
+        dataframe=pathways_dataframe, metadata_table=programs_table, primary_key="id"
     )
     logger.info("----Data loaded into Google Pathways API")
