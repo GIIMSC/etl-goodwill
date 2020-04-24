a7229e62145a43d4dc3b065b96aa0a6fc7745373
Regina Compton <reginafcompton@gmail.com>
2020-04-24 10:49:33 -0400

    refactor: Use SQLAlchemy Core for all testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from time import sleep
 
 import docker
 import pytest
-from sqlalchemy import MetaData, Table, create_engine
+from sqlalchemy import MetaData, Table, Column, create_engine, insert, delete
 from sqlalchemy.dialects.postgresql.json import JSONB
 from sqlalchemy.engine import ResultProxy
 from sqlalchemy.types import TIMESTAMP, String
@@ -21,9 +21,10 @@ from etl.pathways_opt_out import OptOut
 from etl.transformers.dataframe_transformer import DataframeTransformer
 from etl.utils.logger import logger
 
-from tests.models_for_testing import PathwaysProgram, Base
 
 ENGINE = create_engine(SQLALCHEMY_DATABASE_URI)
+# Create a MetaData object, which stores the components of the database being described or reflected
+METADATA_OBJECT = MetaData(bind=ENGINE)
 
 
 class PostgreSQLContainer:
@@ -33,7 +34,6 @@ class PostgreSQLContainer:
     containers so that a database can be injected into tests.
 
     Class Attributes:
-        config (object): A Configuration Factory object.
         container (object): The Docker container object.
         docker_client (object): Docker client.
         db_environment (list): Database environment configuration variables.
@@ -101,15 +101,24 @@ class PostgreSQLContainer:
 
 
 def create_pathways_program_table():
-    """Apply Database Migrations Applies the database migrations to the test
-    database container.
+    """Create a PathwaysProgram table in the database.
 
-    Args:
-        config (object): Configuration object to pull the needed components from.
+    This function instantiates a SQLAlchemy Table object and associates it with 
+    the METADATA_OBJECT. Then, the function waits for Docker to standup the database
+    and calls `create_all`, which calls "CREATE TABLE" for all associated tables.
+    https://docs.sqlalchemy.org/en/13/core/tutorial.html#define-and-create-tables
     """
     table_created = False
     retries = 0
     max_retries = 3
+
+    programs_table = Table(	
+        "pathways_program",	
+        METADATA_OBJECT,	
+        Column("id", String, primary_key=True),	
+        Column("pathways_program", JSONB, nullable=False),	
+        Column("updated_at", TIMESTAMP, nullable=False),	
+    )
 
     while retries < max_retries and table_created is False:
         logger.info(
@@ -118,21 +127,27 @@ def create_pathways_program_table():
             )
         )
         try:
-            Base.metadata.create_all(ENGINE)
+            METADATA_OBJECT.create_all(ENGINE)
             table_created = True
         except Exception:
             retries += 1
             sleep(2)
 
 
+def pathways_program_table():
+    return Table("pathways_program", METADATA_OBJECT, autoload=True)
+
+
 @pytest.fixture(scope="session", autouse=True)
 def database():
     postgres = PostgreSQLContainer()
+
     yield postgres.start_container()
+
     postgres.stop_if_running()
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(pathways_program_table, autouse=True)
 def database_session():
     Session = sessionmaker()
     Session.configure(bind=ENGINE)
@@ -140,7 +155,8 @@ def database_session():
 
     yield session
 
-    session.query(PathwaysProgram).delete()
+    delete_stmt = delete(pathways_program_table())
+    session.execute(delete_stmt)
     session.commit()
     session.close()
 
@@ -250,8 +266,7 @@ def transformer(google_sheet_data):
 
 @pytest.fixture
 def opt_out(google_sheet_data):
-    metadata = MetaData(bind=ENGINE)
-    programs_table = Table("pathways_program", metadata, autoload=True)
+    programs_table = pathways_program_table()
 
     opt_out = OptOut(
         google_sheet_as_list=google_sheet_data,
@@ -266,36 +281,36 @@ def opt_out(google_sheet_data):
 def pathways_programs(database_session):
     """A fixture that adds two programs to the database transaction (i.e.,
     SQLAlchemy session)."""
-    json_ld = {
+    json_ld_one = {
         "@context": "http://schema.org/",
         "@type": "EducationalOccupationalProgram",
         "name": "Certified Nursing Assistant Program",
         "description": "This course helps participants cultivate the attitudes, skills and behaviors of a competent caregiver",
     }
 
-    program_data = {
+    program_data_one = {
         "id": "5f109a01-87c6",
         "updated_at": "2020-03-23 15:10:50",
-        "pathways_program": json.dumps(json_ld),
+        "pathways_program": json.dumps(json_ld_one),
     }
-    program_one = PathwaysProgram(**program_data)
 
-    json_ld = {
+    json_ld_two = {
         "@context": "http://schema.org/",
         "@type": "WorkBasedProgram",
         "name": "Customer Service and Sales Training",
         "description": "Provides training in Customer Service and Sales",
     }
 
-    program_data = {
+    program_data_two = {
         "id": "663dfe-4aca",
         "updated_at": "2020-02-01 1:31:10",
-        "pathways_program": json.dumps(json_ld),
+        "pathways_program": json.dumps(json_ld_two),
     }
-    program_two = PathwaysProgram(**program_data)
 
-    database_session.add(program_one)
-    database_session.add(program_two)
+    # Create a SQLAlchemy Insert object
+    insert_obj = insert(pathways_program_table())
+    insert_obj_with_values = insert_obj.values([program_data_one, program_data_two])
+    database_session.execute(insert_obj_with_values)
     database_session.commit()
 
 
